3197c8fb510f968c5ae55d95d817d7d68ea418a2
OLVAR <diab.mov@gmail.com>
2022-09-22 09:52:26 -0500

    Array and replacement

---
+++ app.js
@@ -0,0 +1,9 @@
+/*const originalStory["There would be no one to live for during those coming years
+ she would live for herself. There would be no powerful will bending hers in that 
+blind persistence with which men and women believe they have a right to impose 
+a private will upon a fellow-creature. A kind intention or a cruel intention made
+ the act seem no less a crime as she looked upon it in that brief moment of 
+illumination."]*/
+
+const userInputs = ["weeks","meaningful"];
+console.log('There would be no one to live for during those coming' , userInputs[0])
